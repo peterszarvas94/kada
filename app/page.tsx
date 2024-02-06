d0bac29d9294087a976a46f1961a56b5f8f80c4f
Szarvas Péter <peterszarvas94@gmail.com>
2024-02-06 12:48:21 +0100

    fix: infinite scroll works on any screen

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { CartContextWrapper } from "@/components/cart-context-wrapper";
 export default function HomePage() {
   return (
     <CartContextWrapper>
-      <main className="relative">
+      <main className="relative min-h-screen mb-10">
         <CartButton />
         <div className="pb-[40px] pt-24 sm:pt-[60px]">
           <h1 className="flex justify-center text-3xl sm:text-[48px] font-[600]">
